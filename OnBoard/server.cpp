edb4020a562e80ed2861eeba004a981a412a6378
guanqp <guanqp@126.com>
2014-07-04 15:28:08 +0800

    继续完善框架。生成两个matrixHS

--- OnBoard/server.cpp
+++ OnBoard/server.cpp
@@ -21,6 +21,7 @@ using namespace std;
 const int MaxSendLength=1000;
 const int MaxRecLength=20;
 const int ListenPort=9031,SendPort=9032;
+const int Color_tracked=0;
 Communication::Udp _udp(ListenPort,false);
 Communication::Udp::UdpPackage package;
 ColorTrack tracker;
@@ -85,7 +86,7 @@ int main (int argc, char **argv)
 	void *capture = capture_open("/dev/video0", Width[config.pixel], Height[config.pixel], PIX_FMT_YUV420P);
 
 	U8* img_hs=new U8[2*Width[config.pixel]*Height[config.pixel]];
-
+    itr_math::Matrix mat_H(Height[config.pixel],Width[config.pixel]),mat_S(Height[config.pixel],Width[config.pixel]);
 	if (!capture) {
 		fprintf(stderr, "ERR: can't open '/dev/video0'\n");
 		exit(-1);
@@ -103,25 +104,31 @@ int main (int argc, char **argv)
 		if(_udp.Receive(RecBuf,MaxRecLength))
         {
             //TODO:使用SSP进行解包
-            ssp_obj.Init(0xA5 ,0x5A ,NULL);
+            ssp_obj.Init(0xA5 ,0x5A ,NULL);//串口发送函数 代替 NULL
             ssp_obj.ProcessFunction[0]=&SSPReceivefuc;
             ssp_obj.ProcessRawByte((U8*)RecBuf,MaxRecLength);
         }
-        // if (!start)
-        //     continue;
+         if (!start)
+             continue;
 
         //TODO:获取图像，进行RGB，HSL的转换
 		Picture pic;
 		capture_get_picture(capture, &pic);
         yuv2rgb_obj.doyuv2hsl(Width[config.pixel],Height[config.pixel],pic.data[0],pic.data[1],pic.data[2],
                                                     img_hs,img_hs+Width[config.pixel]*Height[config.pixel]);
-
+        //将HS转存入矩阵中
+        int _index=0, _size=Height[config.pixel]*Width[config.pixel];
+        for(int i=0; i<_size; i++){
+                mat_H[_index]=img_hs[_index];
+                mat_S[_index]=img_hs[_index+_size];
+                _index++;
+        }
 
         //TODO：进行跟踪
+        std::vector<itr_vision::Block> blocklist;
         if(track)
         {
-//            list=tracker.Track(*,*);
-
+            blocklist=tracker.Track(mat_H,mat_S,Color_tracked);
         }
 
 		// 压缩图像
@@ -132,7 +139,7 @@ int main (int argc, char **argv)
 		if (rc < 0) continue;
 
 		//用SSP封装，包括图像和跟踪结果
-
+        //数据最大长度226？
 
 		// 发送结果
 		package.pbuffer=(char*)outdata;
@@ -145,7 +152,7 @@ int main (int argc, char **argv)
 
 	vc_close(encoder);
 	capture_close(capture);
-
+    delete[] img_hs;
 	return 0;
 }
 
