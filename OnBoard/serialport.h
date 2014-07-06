c99b6e73524cae972b1ee51c4377dbbf1469e6ec
ghdawn <ghdawn31@gmail.com>
2014-07-06 22:39:37 +0800

    完成了图像部分的联合调试

---
+++ OnBoard/serialport.h
@@ -0,0 +1,18 @@
+#ifndef SERIALPORT_H
+#define SERIALPORT_H
+
+
+class SerialPort
+{
+
+public:
+    SerialPort();
+    int fd;
+    void Init(char * name, int baudrate);
+    void Close();
+    int Send(unsigned char *Data, int length);
+    int Receive(unsigned char *Data, int length);
+//protected:
+    //virtual void run();
+};
+#endif
--- OnBoard/server.cpp
+++ OnBoard/server.cpp
@@ -33,46 +33,45 @@ ColorTrack tracker;
 
 Config config;
 
-
-bool start=true;
-bool track=true;
+U8 mode=2;
 ///SSP接受数据，进行命令解析
 void SSPReceivefuc(itr_protocol::StandSerialProtocol *SSP, itr_protocol::StandSerialFrameStruct *SSFS,U8 *Package,S32 PackageLength)
 {
     switch(Package[0])
     {
-        case 2:
-            start=false;
-            track=false;
+        case 0x31:
+            mode=Package[1];
             break;
-        case 3:
-            start=true;
-            track=false;
-            //直接转发
+        case 0x32:
+
             break;
-        case 4:
-            start=true;
-            track=true;
-            break;
-        case 5:
+        case 0x33:
             config.color=Package[1];
             config.result=Package[2];
             break;
-        case 6:
-            config.fps=Package[1];
+        case 0x34:
+             config.fps=Package[1];
             config.pixel=Package[2];
+            break;
+        //直接转发
+        default:
             break;
     }
 }
 
 S32 SendResultPrepare(U8* Buffer,S32 Length)
 {
-	MemoryCopy(SendBuf,Buffer,Length);
-	// for (int i = 0; i < Length; ++i)
-	// {
-	// 	printf("%x ",Buffer[i]);
-	// }
-	MemoryCopy(SendBuf+Length,(void*)imgCompressData,imgLength);
+	memcpy(SendBuf,Buffer,Length);
+	for (int i = 0; i < Length; ++i)
+	{
+		printf("%X ",Buffer[i]);
+	}
+	printf("\n");
+	for (int i = 0; i < Length; ++i)
+	{
+		printf("%X ",Buffer[i]);
+	}
+	memcpy(SendBuf+Length,(void*)imgCompressData,imgLength);
     SendLength=Length+imgLength;
     return SendLength;
 }
@@ -85,7 +84,8 @@ void Init()
     config.pixel=0;
     config.fps=30;
 
-    udpPackage.IP="0.0.0.0";
+    udpPackage.IP="192.168.1.112";
+    // udpPackage.IP="0.0.0.0";
     udpPackage.port=SendPort;
 
     itr_math::MathObjStandInit();
@@ -99,7 +99,8 @@ int main (int argc, char **argv)
 {
     Init();
     U8 tempbuff[100];
-
+    F32 fps,x,y,Area;
+    TimeClock clock;
     yuv2hsl yuv2hsl_obj;///用于yuv到hls转换
     int _size=Height[config.pixel]*Width[config.pixel];
 
@@ -130,11 +131,11 @@ int main (int argc, char **argv)
             //使用SSP进行解包
             sspUdp.ProcessRawByte((U8 *)RecBuf,MaxRecLength);
         }
-        if (!start)
+        if (mode==0)
         {
             continue;
         }
-
+        clock.Tick();
         //获取图像，进行RGB，HSL的转换
         Picture pic;
         capture_get_picture(capture, &pic);
@@ -151,11 +152,16 @@ int main (int argc, char **argv)
 
         //进行跟踪
         std::vector<itr_vision::Block> blocklist;
-        if(track)
+        if(mode==2)
         {
             blocklist=tracker.Track(mat_H,mat_S,ColorTable[config.color]);
+            x=y=Area=-1;
             if(blocklist.size()>0)
-                printf("%d %d %d\n",blocklist[0].x,blocklist[0].y,blocklist[0].Area);
+               { printf("%f %f %f\n",blocklist[0].x,blocklist[0].y,blocklist[0].Area);
+               x=blocklist[0].x;
+               y=blocklist[0].y;
+               Area=blocklist[0].Area;
+               }
         }
 
         // 压缩图像
@@ -166,20 +172,30 @@ int main (int argc, char **argv)
         {
             continue;
         }
-
+        fps=1000/clock.Tick();
         //用SSP封装，包括图像和跟踪结果
         //数据最大长度226？
-        if(config.result==0&&blocklist.size()>0)
+        SendLength=0;
+        if(config.result==0)
         {
-            tempbuff[0]=(start)?1:0;
-            tempbuff[1]=(track)?1:0;
-            MemoryCopy(tempbuff+2,(void*)&blocklist[0],sizeof(blocklist[0]));
-            sspUdp.SSPSendPackage(0,tempbuff,sizeof(blocklist[0])+2);
+        	int offset=0;
+        	tempbuff[offset++]=0x30;
+            tempbuff[offset++]=mode;
+            memcpy(tempbuff+offset,(void*)&fps,4);
+            offset+=4;
+            memcpy(tempbuff+offset,(void*)&x,4);
+            offset+=4;
+            memcpy(tempbuff+offset,(void*)&y,4);
+            offset+=4;
+            memcpy(tempbuff+offset,(void*)&Area,4);
+            offset+=4;
+            sspUdp.SSPSendPackage(0,tempbuff,offset);
         }
 
         // 发送结果
         udpPackage.pbuffer=SendBuf;
         udpPackage.len=SendLength;
+        printf("Len:%d\n",SendLength );
         _udp.Send(udpPackage);
 
         // 等
