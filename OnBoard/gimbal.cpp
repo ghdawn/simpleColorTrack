440424548014f60fc5119693a482f208ab29e524
ghdawn <ghdawn31@gmail.com>
2014-07-07 15:46:04 +0800

    增加了云台控制文件

--- OnBoard/gimbal.cpp
+++ OnBoard/gimbal.cpp
@@ -11,6 +11,7 @@ float kdy = 0;
 float U0 = 160;
 float V0 = 120;
 
+U8 Buffer[];
 S32 SendResultPrepare(U8* Buffer,S32 Length)
 {
     return SendLength;
@@ -18,12 +19,11 @@ S32 SendResultPrepare(U8* Buffer,S32 Length)
 
 void GimbalInit()
 {
-    sspObj.Init(0xA5 ,0x5A ,SendResultPrepare);//串口发送函数 代替 NULL
+    sspObj.Init(0xA5 ,0x5A ,SendResultPrepare);
 }
 
 void GimbalControl(float x,float y,char* ControlData,int length)
 {
-
     float omegax;
     float omegay;
     omegax = (X[0]-U0)*kpx + X[2]*kdx;
