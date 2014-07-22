904bc48064a533884f7133ee6096eec9031fcd2f
ghdawn <ghdawn31@gmail.com>
2014-07-22 16:42:06 +0800

    多线程版本测试成功

--- OnBoard/server.cpp
+++ OnBoard/server.cpp
@@ -65,7 +65,8 @@ F32 fps,x,y,Area;
 */
 U8 mode=2;
 
-
+pthread_mutex_t mutexTrack,mutexCompress;
+bool newImg=false,newResult=false;
 ///SSP接受数据，进行命令解析
 void SSPReceivefuc(itr_protocol::StandSerialProtocol *SSP, itr_protocol::StandSerialFrameStruct *SSFS,U8 *Package,S32 PackageLength)
 {
@@ -95,11 +96,11 @@ void SSPReceivefuc(itr_protocol::StandSerialProtocol *SSP, itr_protocol::StandSe
 S32 SSPSend(U8* Buffer,S32 Length)
 {
     memcpy(SendBuf,Buffer,Length);
-    for (int i = 0; i < Length; ++i)
-    {
-        printf("%X ",Buffer[i]);
-    }
-    printf("\n");
+    // for (int i = 0; i < Length; ++i)
+    // {
+    //     printf("%X ",Buffer[i]);
+    // }
+    // printf("\n");
     memcpy(SendBuf+Length,(void*)imgCompressData,imgLength);
     SendLength=Length+imgLength;
     return SendLength;
@@ -113,7 +114,7 @@ void Init()
     config.pixel=0;
     config.fps=30;
 
-    udpPackage.IP="192.168.0.116";
+    udpPackage.IP="192.168.0.110";
     // udpPackage.IP="0.0.0.0";
     udpPackage.port=SendPort;
 
@@ -145,15 +146,30 @@ void* x264_thread(void* name)
         exit(-1);
     }
     Picture *pic;
+    TimeClock tc;
+    tc.Tick();
     while(1)
     {
+        
         // 压缩图像
         pic=yuvBuffer.GetBufferToRead();
+        if (pic==NULL)
+        {
+            // usleep(10);
+            continue;
+        }        
+        pthread_mutex_lock(&mutexCompress);
         int rc = vc_compress(encoder, pic->data, pic->stride, &imgCompressData, &imgLength);
+        if(rc>0)
+            newImg=true;
+        
+        yuvBuffer.SetBufferToWrite(pic);
         if (rc < 0)
         {
             continue;
         }
+        pthread_mutex_unlock(&mutexCompress);
+        printf("Compress OK at time=%d\n",tc.Tick());
     }
 
     vc_close(encoder);
@@ -173,15 +189,29 @@ void* camera_thread(void *name)
     ///用于yuv到hls转换
     yuv2hsl yuv2hsl_obj;
     F32* img_hs;
+    TimeClock tc;
+    tc.Tick();
     while(1)
     {
         pic=yuvBuffer.GetBufferToWrite();
+        if (pic==NULL)
+        {
+            // usleep(10);
+            continue;
+        }
         capture_get_picture(capture, pic);
         yuvBuffer.SetBufferTRead(pic);
-        img_hs=matBuffer.GetBufferToWrite();
+        img_hs=matBuffer.GetBufferToWrite();    
+        while(img_hs==NULL)
+        {
+            // usleep(10);
+            img_hs=matBuffer.GetBufferToWrite();    
+        }
+        
         yuv2hsl_obj.doyuv2hsl(_width,_height,pic->data[0],pic->data[1],pic->data[2],
           img_hs,img_hs+_size);
         matBuffer.SetBufferTRead(img_hs);
+        printf("New Img OK at time=%d\n",tc.Tick());
     }
 
     capture_close(capture);
@@ -192,6 +222,8 @@ void* track_thread(void* name)
     
     F32* img_hs;
     int x_ever=0,y_ever=0,color_counter=0;
+    TimeClock tc;
+    tc.Tick();
     while(1)
     {
         img_hs=matBuffer.GetBufferToRead();
@@ -201,6 +233,8 @@ void* track_thread(void* name)
         }
         if(mode==2)
         {
+            pthread_mutex_lock(&mutexTrack);
+
             x_ever=0,y_ever=0,color_counter=0;
             unsigned int ind=0;
             for(int i=0; i<_height; i++)
@@ -220,11 +254,16 @@ void* track_thread(void* name)
             {
                 x_ever/=color_counter;
                 y_ever/=color_counter;
-                x=x_ever;
-                y=y_ever;
-                Area=color_counter;
-                GimbalControl( x, y,&ControlData,controllength);
             }
+            x=x_ever;
+            y=y_ever;
+            Area=color_counter;
+            fps=1000/tc.Tick();
+            GimbalControl( x, y,&ControlData,controllength);
+printf("Track OK, at fps=%f\n", fps);
+            // newResult=true;
+            pthread_mutex_unlock(&mutexTrack);
+            
         }
         matBuffer.SetBufferToWrite(img_hs);
     }
@@ -239,7 +278,10 @@ int main (int argc, char **argv)
     pthread_t tidx264,tidcam,tidtrack; 
 
     U8 tempbuff[100];
-
+    TimeClock tc;
+    tc.Tick();
+    pthread_mutex_init(&mutexTrack,NULL);
+    pthread_mutex_init(&mutexCompress,NULL);
     pthread_create(&tidcam, NULL, camera_thread, (void *)( "Camera" )); 
     pthread_create(&tidx264, NULL, x264_thread, (void *)( "x264" ));
     pthread_create(&tidtrack, NULL, track_thread, (void *)( "Track" ));
@@ -247,7 +289,6 @@ int main (int argc, char **argv)
 
     for (; ; )
     {
-
         if(_udp.Receive(RecBuf,MaxRecLength))
         {
             //使用SSP进行解包
@@ -260,35 +301,41 @@ int main (int argc, char **argv)
         //获取图像，进行RGB，HSL的转换
         //将HS转存入矩阵中
 
-
         //用SSP封装，包括图像和跟踪结果
         SendLength=0;
-        if(config.result==0)
+        if(newImg && (config.result==0))
         {
-            int offset=0;
-            tempbuff[offset++]=0x40;
-            tempbuff[offset++]=mode;
-            memcpy(tempbuff+offset,(void*)&fps,4);
-            offset+=4;
-            memcpy(tempbuff+offset,(void*)&x,4);
-            offset+=4;
-            memcpy(tempbuff+offset,(void*)&y,4);
-            offset+=4;
-            memcpy(tempbuff+offset,(void*)&Area,4);
-            offset+=4;
-            sspUdp.SSPSendPackage(0,tempbuff,offset);
+            pthread_mutex_lock(&mutexCompress);
+            pthread_mutex_lock(&mutexTrack);
+            {
+                int offset=0;
+                tempbuff[offset++]=0x40;
+                tempbuff[offset++]=mode;
+                memcpy(tempbuff+offset,(void*)&fps,4);
+                offset+=4;
+                memcpy(tempbuff+offset,(void*)&x,4);
+                offset+=4;
+                memcpy(tempbuff+offset,(void*)&y,4);
+                offset+=4;
+                memcpy(tempbuff+offset,(void*)&Area,4);
+                offset+=4;
+                sspUdp.SSPSendPackage(0,tempbuff,offset);
+            }
+            pthread_mutex_unlock(&mutexCompress);
+            pthread_mutex_unlock(&mutexTrack);
+            // 发送结果
+            udpPackage.pbuffer=SendBuf;
+            udpPackage.len=SendLength;
+            _udp.Send(udpPackage);
+
+            newImg=false;
+            newResult=false;
+            printf("Send OK at time=%d\n",tc.Tick());
+
         }
+        // usleep(30);
 
-        // 发送结果
-        udpPackage.pbuffer=SendBuf;
-        udpPackage.len=SendLength;
-        _udp.Send(udpPackage);
-
-        // 等
-        // usleep(tosleep);
-    }
-
-    
+    }    
     
     return 0;
 }
