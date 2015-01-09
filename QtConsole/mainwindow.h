2d352cc72faa619dfeec49c04d6771f5d73064da
ghdawn <ghdawn31@gmail.com>
2015-01-09 11:26:44 +0800

    增加了关闭跟踪程序的功能

--- QtConsole/mainwindow.h
+++ QtConsole/mainwindow.h
@@ -52,6 +52,7 @@ private slots:
     void on_pushButton_3_clicked();
     void on_pushButton_4_clicked();
     void on_pushButton_5_clicked();
+    void on_btExit_clicked();
 };
 
 #endif // MAINWINDOW_H
